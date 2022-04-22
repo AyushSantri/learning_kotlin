066a6f07539edae74eefe3b9b86d4ff1fe112623
AyushSantri <ayushsantri222@gmail.com>
2022-04-23 01:00:26 +0530

    learned about onclick in button and extracting the text from textView

--- app/src/main/java/com/example/larningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/larningkotlin/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.larningkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        var name = nameInput.editableText.toString()
+        Toast.makeText(this, "hey there $name", Toast.LENGTH_LONG).show()
     }
 }
