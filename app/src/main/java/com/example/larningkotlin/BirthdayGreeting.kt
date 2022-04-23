1b96be6966b3ea981ef3aff8b1d7bce71867e907
AyushSantri <ayushsantri222@gmail.com>
2022-04-23 21:58:22 +0530

    recieved the name passed in MainActivity from BirthdayGreeting.kt

--- app/src/main/java/com/example/larningkotlin/BirthdayGreeting.kt
+++ app/src/main/java/com/example/larningkotlin/BirthdayGreeting.kt
@@ -7,5 +7,9 @@ class BirthdayGreeting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting)
+
+        //captured the name from main.kt
+        //getStringExtra is used because we passed string in this activity
+        val name = intent.getStringExtra("name")
     }
 }
