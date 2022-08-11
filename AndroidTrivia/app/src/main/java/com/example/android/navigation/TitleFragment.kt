bc4044df47332f3fd76001581c38fa84029aaf8a
Ayush Santri <ayushsantri@flash.tech>
2022-08-11 20:54:01 +0530

    implemented menu in TitleFragment.kt

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,15 +1,18 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitle2Binding
 
-class TitleFragment : Fragment() {
+class TitleFragment : Fragment(), MenuProvider {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +23,20 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
         }
+
+        val menuHost : MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return  binding.root
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            menuItem,
+            requireView().findNavController())
     }
 }
