2b4393e7dedfe4265aff93a24f300a9e4d5ea4d4
Ayush Santri <ayushsantri@flash.tech>
2022-08-11 18:55:36 +0530

    found the navController from NavHostFragment

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,7 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,5 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
     }
 }
