0f21738c0abe55f6de2588b9b8157647e4aba443
Ayush Santri <ayushsantri@flash.tech>
2022-08-12 14:25:58 +0530

    showed arguments in GameWonFragment.kt

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -28,7 +29,7 @@ import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
@@ -36,6 +37,10 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
+        val args = GameWonFragmentArgs.fromBundle(arguments)
+
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
 
         return binding.root
     }
